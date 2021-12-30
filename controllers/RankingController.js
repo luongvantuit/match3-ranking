a65f9d8a5bea893235d477b1dd797ccb6fae3864
Lương Văn Tú <luongvantuit@gmail.com>
2021-12-30 14:52:21 +0700

    controller score & ranking

---
+++ controllers/RankingController.js
@@ -0,0 +1,19 @@
+'use strict'
+const express = require('express');
+
+class RankingController {
+
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @returns {Promise<void>}
+ */
+RankingController.prototype.index = async function (req, res) {
+
+}
+
+
+module.exports = new RankingController;
---
+++ controllers/ScoreController.js
@@ -0,0 +1,20 @@
+'use strict'
+const express = require('express')
+
+class ScoreController {
+
+}
+
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @returns {Promise<void>}
+ */
+ScoreController.prototype.index = async function (req, res) {
+
+
+}
+
+
+module.exports = new ScoreController;
