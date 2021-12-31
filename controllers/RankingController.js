94f8a1f9bc2f8eb596501e395a87942b00b01efd
Lương Văn Tú <luongvantuit@gmail.com>
2021-12-31 23:12:45 +0700

    define params ranking controller

--- controllers/RankingController.js
+++ controllers/RankingController.js
@@ -12,7 +12,17 @@ class RankingController {
  * @returns {Promise<void>}
  */
 RankingController.prototype.index = async function (req, res) {
+    const { level, limit, page } = await req.params;
+}
 
+/**
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @returns {Promise<void>}
+ */
+RankingController.prototype.show = async function (req, res) {
+    const { uid, level, limit, page } = await req.params;
 }
 
 
--- routes/API.js
+++ routes/API.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const RankingController = require("../controllers/RankingController");
 const ScoreController = require("../controllers/ScoreController");
 
 
@@ -7,5 +8,8 @@ const API = express.Router();
 API.get('/score/:uid/:level', ScoreController.index);
 API.get('/score/:uid', ScoreController.index);
 API.get('/score/:uid', express.json(), ScoreController.perform);
+
+API.get('/ranking/:uid/:level', RankingController.show);
+API.get('/ranking/:level', ScoreController.index);
 
 module.exports = API;
